a838a4c96eeb7df636e48b68cbcd378c7ab917a4
MNOK2 <mnok2@outlook.jp>
2022-11-20 00:53:16 +0900

    計算テスト関数の実装

--- App.java
+++ App.java
@@ -3,21 +3,17 @@ import multiple_precision.Number;
 
 public class App {
     public static void main(String[] args) {
-        testCalculation(10000, 10);
+        testCalculationOnce(6);
+        System.out.println();
+        testCalculationRepeating(10000, 10);
     }
 
-    private static boolean testCalculation(int calculationCount, int progressPrintCount) {
-        if (calculationCount < 0) throw new IllegalArgumentException();
-        if (calculationCount < progressPrintCount) throw new IllegalArgumentException();
 
-        final int CALCULATION_DIGITS_COUNT = 5;
-        final int CALCULATION_VALUE_ORIGIN = -100000;
-        final int CALCULATION_VALUE_RANGE = 200000;
+    private static void testCalculationOnce(int digitsCount) {
+        if (digitsCount < 0) throw new IllegalArgumentException();
 
-        Random random = new Random();
-
-        Number numberA = Number.random(CALCULATION_DIGITS_COUNT);
-        Number numberB = Number.random(CALCULATION_DIGITS_COUNT / 2);
+        Number numberA = Number.random(digitsCount);
+        Number numberB = Number.random(digitsCount);
 
         System.out.println("a = " + numberA);
         System.out.println("b = " + numberB);
@@ -27,16 +23,25 @@ public class App {
         System.out.println("a * b = " + numberA.mul(numberB));
         System.out.println("a / b = " + numberA.div(numberB));
         System.out.println("a % b = " + numberA.mod(numberB));
+    }
 
-        System.out.println();
+    private static boolean testCalculationRepeating(int calculationCount, int progressPrintCount) {
+        if (calculationCount < 0) throw new IllegalArgumentException();
+        if (calculationCount < progressPrintCount) throw new IllegalArgumentException();
+
+        final int CALCULATION_VALUE_ORIGIN = -100000;
+        final int CALCULATION_VALUE_RANGE = 200000;
+
+        Random random = new Random();
+        long startTime = System.currentTimeMillis();
 
         int progressInterval = calculationCount / progressPrintCount;
         System.out.println(String.format("=== 連続計算を開始: %d回 ===", calculationCount));
         for (int i = 0; i < calculationCount; i++) {
             int intA = random.nextInt(CALCULATION_VALUE_RANGE) + CALCULATION_VALUE_ORIGIN;
             int intB = random.nextInt(CALCULATION_VALUE_RANGE) + CALCULATION_VALUE_ORIGIN;
-            numberA = new Number(intA);
-            numberB = new Number(intB);
+            Number numberA = new Number(intA);
+            Number numberB = new Number(intB);
 
             if ((i + 1) % progressInterval == 0) System.out.println(String.format("%d回目の計算中...", i + 1, calculationCount));
 
@@ -64,8 +69,10 @@ public class App {
                 return false;
             }
         }
+
+        long endTime = System.currentTimeMillis();
         
-        System.out.println("正常に終了しました。（無敵）");
+        System.out.println(String.format("正常に終了しました。（計算時間: %.3fs）", (endTime - startTime) / 1000.0f));
         return true;
     }
 }
